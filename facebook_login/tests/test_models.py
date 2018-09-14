6ffd548c07d05a7e48f4df1fc7948ded710337b0
Martin Brochhaus <mbrochh@gmail.com>
2018-09-14 15:04:35 +0800

    Adds test for FacebookAccount.__str__()

--- facebook_login/tests/test_models.py
+++ facebook_login/tests/test_models.py
@@ -11,3 +11,10 @@ class TestFacebookAccount(object):
     def test_model(self):
         obj = mixer.blend('facebook_login.FacebookAccount')
         assert obj.pk, ('Should save an instance')
+
+    def test_str(self):
+        user = mixer.blend('auth.User', email='test@example.com')
+        obj = mixer.blend('facebook_login.FacebookAccount')
+        result = str(obj)
+        expected = str(user)
+        assert result == expected, ('Should return the user email')
