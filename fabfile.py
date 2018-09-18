a540dba898d23432a2e25ddae5efa60bd5a900ad
Martin Brochhaus <mbrochh@gmail.com>
2018-09-18 10:32:32 +0800

    chore: Consolidates term + html coverage into one fab task `fab test`

--- fabfile.py
+++ fabfile.py
@@ -7,12 +7,5 @@ def test(c):
     run('py.test -s'
         ' --cov facebook_login'
         ' --cov-report term'
-        ' --ds=facebook_login.tests.test_settings')
-
-
-@task
-def test_html(c):
-    run('py.test -s'
-        ' --cov facebook_login'
         ' --cov-report html'
         ' --ds=facebook_login.tests.test_settings')
